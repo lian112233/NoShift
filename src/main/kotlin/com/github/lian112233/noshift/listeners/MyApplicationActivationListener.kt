d72f3d39ad9abb7fa310f6a6cfd8142d06eaaae6
GitHub Action <action@github.com>
2024-12-27 09:43:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/lian112233/noshift/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/lian112233/noshift/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.lian112233.noshift.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
